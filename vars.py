aaa9cc9a583d81707a94eee98526f7b6100bb10c
iryme786 <manishamandall7777@gmail.com>
2025-06-28 14:06:41 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,17 @@
+#lexm_8
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "29320367"))
+API_HASH = environ.get("API_HASH", "f8083ef747c160b7eb41095454f59f92")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "6559064772"))
+CREDIT = "@lexm_8"
+AUTH_USER = os.environ.get('AUTH_USERS', '6559064772').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
